21704d74e3e8129867de407fd918909a66d55c94
unknown <ezzheva.ira@yandex.ru>
2023-10-04 22:42:04 +0300

    controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,9 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const NotFoundError = require('../errors/NotFoundError');
-const BadRequest = require('../errors/BadRequest');
-const ConflictError = require('../errors/ConflictError');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const NotFoundError = require("../errors/NotFoundError");
+const BadRequest = require("../errors/BadRequest");
+const ConflictError = require("../errors/ConflictError");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -11,32 +11,49 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.status(200).send({ token });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
+        { expiresIn: "7d" }
+      );
+      res.status(200).send({
+        token,
+        user: {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+        },
+      });
     })
     .catch(next);
 };
 
 /** создание пользователя */
 module.exports.createUser = (req, res, next) => {
-  const {
-    name, email, password,
-  } = req.body;
-  bcrypt.hash(password, 10)
-    .then((hash) => User.create({
-      name, email, password: hash,
-    }))
-    .then(() => res.status(201).send(
-      {
-        name, email,
-      },
-    ))
+  const { name, email, password } = req.body;
+  bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      User.create({
+        name,
+        email,
+        password: hash,
+      })
+    )
+    .then(() =>
+      res.status(201).send({
+        name,
+        email,
+      })
+    )
     .catch((err) => {
       if (err.code === 11000) {
-        return next(new ConflictError('Пользователь с таким email уже существует'));
+        return next(
+          new ConflictError("Пользователь с таким email уже существует")
+        );
       }
-      if (err.name === 'ValidationError') {
-        return next(new BadRequest('Некорректные данные пользователя'));
+      if (err.name === "ValidationError") {
+        return next(new BadRequest("Некорректные данные пользователя"));
       }
       return next(err);
     });
@@ -46,17 +63,23 @@ module.exports.createUser = (req, res, next) => {
 module.exports.updateUser = (req, res, next) => {
   const { name, email } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { name, email }, { new: true, runValidators: true })
-    .orFail(new NotFoundError('Пользователь с указаным _id не найден'))
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, email },
+    { new: true, runValidators: true }
+  )
+    .orFail(new NotFoundError("Пользователь с указаным _id не найден"))
     .then((user) => {
       res.status(200).send(user);
     })
     .catch((err) => {
       if (err.code === 11000) {
-        return next(new ConflictError('Пользователь с таким email уже существует'));
+        return next(
+          new ConflictError("Пользователь с таким email уже существует")
+        );
       }
-      if (err.name === 'ValidationError') {
-        return next(new BadRequest('Некорректные данные пользователя'));
+      if (err.name === "ValidationError") {
+        return next(new BadRequest("Некорректные данные пользователя"));
       }
       return next(err);
     });
@@ -65,13 +88,13 @@ module.exports.updateUser = (req, res, next) => {
 /** текущий пользователь */
 module.exports.getUserMe = (req, res, next) => {
   User.findById(req.user._id)
-    .orFail(new NotFoundError('Пользователь не найден'))
+    .orFail(new NotFoundError("Пользователь не найден"))
     .then((user) => {
       res.status(200).send(user);
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return next(new BadRequest('Некорректные данные пользователя'));
+      if (err.name === "ValidationError") {
+        return next(new BadRequest("Некорректные данные пользователя"));
       }
       return next(err);
     });
